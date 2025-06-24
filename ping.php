ec53dde71df7959f87d4bf627306f1f245b77c7c
AffanRifai <affanrifai268@gmail.com>
2025-06-24 20:01:33 +0700

    commit

--- ping.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-echo "pong"; // cukup kirim teks sederhana untuk hitung waktu respons
-?>
--- upload.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Kita tidak perlu memproses file, cukup terima data POST untuk simulasi upload
-http_response_code(200);
-echo "upload received";
-?>
